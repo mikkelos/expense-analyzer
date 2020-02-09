74e6b2bbe8128b7bf79de06b11fb24bdbbbd2b08
mikkelos <mikkel.treu@gmail.com>
2020-02-09 13:57:07 +0100

    Update function and add get_main_categories

--- gcp_interactions/datastore.py
+++ gcp_interactions/datastore.py
@@ -16,14 +16,16 @@ OVERLAPPING_ALLOCATION_THRESHOLD = 0.3
 
 # The entity kind in datastore to query to find previous assignments
 DATASTORE_KIND_CATEGORY_ASSIGNMENT = "category_item_mapping"
+DATASTORE_KIND_CATEGORIES = "category"
 
 """ SESSION VARIABLES """
-user_name = "testuser"
 
 debug = False
 developing = True
 local_run = False
 
+
+# This is only used for local development:
 key_path = "/Volumes/GoogleDrive/My Drive/00. My Documents/03. Internt/24. Expense analyzer/config_files/expense-analyzer-260008-0cac2ecd3671.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 credentials = service_account.Credentials.from_service_account_file(
@@ -36,10 +38,13 @@ datastore_client = datastore.Client(
 )
 
 
+datastore_client = datastore.Client()
+
+
 """ STORAGE BUCKET """
 
 
-def upload_blob(file):
+def upload_blob(file, user):
     """ Uploads a file to the bucket."""
     if local_run:
         storage_client = storage.Client(
@@ -50,6 +55,12 @@ def upload_blob(file):
 
     bucket = storage_client.bucket(UNPROCESSED_BUCKET_NAME)
     blob = bucket.blob(file.filename)
+
+    # Add the uploading user to the metadata
+    metadata = {"uploaded_by": user}
+    blob.metadata = metadata
+
+    # Upload file
     blob.upload_from_file(file)
 
     print('Blob {} uploadted to bucket {}.'.format(
@@ -61,7 +72,7 @@ def upload_blob(file):
 """ DATASTORE """
 
 
-def missing_category(max_records):
+def get_missing_category(max_records):
     """
     :param max_records (int): number of items to return
     :return: a number of unclassified items from the category assignment
@@ -74,6 +85,47 @@ def missing_category(max_records):
     # Fetch first X results
     q_result = query.fetch(limit=max_records)
     return list(q_result)
+
+
+def get_all_categories():
+    """ Get all the categories in the db
+    :return: key:value pairs from the DB containing the categories
+    """
+    query = datastore_client.query(kind=DATASTORE_KIND_CATEGORIES)
+    q_result = query.fetch()
+
+    categories = {}
+    # Add the dummy value for unassigned
+    categories[-1] = "Ikke tildelt"
+
+    for entity in list(q_result):
+        key = entity.key.id_or_name
+        category = entity["cat_name"]
+        categories[key] = category
+
+    return categories
+
+
+def get_main_categories(categories):
+    """ Given a list of categories, returns the top level categories with name"""
+    main_categories = {}
+    for key in categories.keys():
+        if len(str(key)) <= 2:
+            main_categories[key] = categories[key]
+    return main_categories
+
+
+def update_item_category(updates):
+    """ Does a bulk update of item-category mappings
+    :param updates (dict): key: partial key to the item name, value: new category
+    """
+
+    # {'avokado modnet 2pk': 804}
+    for item in updates:
+        key = datastore_client.key('category_item_mapping', item)
+        task = datastore_client.get(key)
+        task["cat_id"] = updates[item]
+        datastore_client.put(task)
 
 
 def upload_categories():
