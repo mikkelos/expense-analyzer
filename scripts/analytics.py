8cbcd1adeb8d9514f5c0814435d840d17fdda36a
mikkelos <mikkel.treu@gmail.com>
2020-04-26 12:13:38 +0200

    Add new function to group on month

--- scripts/analytics.py
+++ scripts/analytics.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from datetime import date
+import calendar
 
 """ Dataframe contains columns:
 unit_price_net
@@ -44,6 +46,50 @@ def expenses_by_level(df, agg_level=2):
 
     # Group on main category
     df = df.groupby(["trans_year", "trans_month", "main_cat"]).sum()
+    df = df.round(1)
+
+    return df
+
+
+def expenses_by_month(df, num_months_back=12):
+    """Takes a df of expenses, groups them on month and filters
+    input:
+        df (pd.DataFrame): dataframe of transactions to group and filter
+        num_months_back (int): default 12. Number of full months back to include data for
+
+    returns:
+        df (pd.DataFrame): filtered and grouped dataframe
+    """
+
+    # Extract year and month
+    df["trans_year"] = df["trans_date"].dt.year
+    df["trans_month"] = df["trans_date"].dt.month
+
+    # Filter to last n num_months_back
+    current_date = date.today()
+    start_month = current_date.month - num_months_back + 1
+    start_year = current_date.year
+
+    if start_month == 0:
+        start_month = 1
+    elif start_month <= 0:
+        start_month += 13
+        start_year -= 1
+
+    from_date = pd.to_datetime(date(start_year, start_month, 1)).tz_localize("UTC")
+    df = df[df["trans_date"] >= from_date]
+
+    # Select relevant columns
+    df = df[["trans_year", "trans_month", "price_gross"]]
+
+    # Add month abbrev
+    df['trans_month_short'] = df['trans_month'].apply(lambda x: calendar.month_abbr[x])
+
+    # Cast all columns to numeric, making errors NaN
+    df["price_gross"] = pd.to_numeric(df["price_gross"], errors="coerce")
+
+    # Group on main category
+    df = df.groupby(["trans_year", "trans_month", "trans_month_short"]).sum()
     df = df.round(1)
 
     return df
